7d3638168e4c30e5ccf798440126a367947cc5ba
Abhinav Kumar Gupta <84512702+alhad-balak@users.noreply.github.com>
2022-04-15 03:57:36 +0530

    Dashborad complete

---
+++ src/components/data/UserData.js
@@ -0,0 +1,8 @@
+
+const userData = {
+    station_code: 40,
+    name: "Abhinav",
+    profile_key: "urlhttps://picsum.photos/200"
+}
+
+export default userData
---
+++ src/components/layout/Dashboard.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Dashboard.css'
+import userData from '../data/UserData'
+
+function Dashboard() {
+  return (
+    <div className="dashborad">
+
+      <div className="DashName">Edvora</div>
+      <div className="Profile">
+        <div className="profileName">{userData.name}</div>
+        <div className="profileImage">
+          <img src={userData.image} alt="" className="top-profile-image" />
+        </div>
+      </div>
+    </div>
+
+  )
+}
+
+export default Dashboard
