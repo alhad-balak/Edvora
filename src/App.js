2c4adc353aea189b173e95da43ac70d4a2d2a8d8
Abhinav Kumar Gupta <84512702+alhad-balak@users.noreply.github.com>
2022-04-15 20:28:11 +0530

    Received API data in Array

--- src/App.js
+++ src/App.js
@@ -119,19 +119,53 @@ function App() {
     });
   }, []);
   // console.log(Object.values(userData)[1]); 
-  
+  var uData = {
+    station_code: Object.values(userData)[0],
+    name: Object.values(userData)[1],
+    url: Object.values(userData)[2]
+
+  };
+
+
   React.useEffect(() => {
     axios.get("https://assessment.api.vweb.app/rides").then((response) => {
       setRidesData(response.data);
     });
   }, []);
-  console.log(Object.values(ridesData)[2].id); 
-  
+  // console.log(Object.values(ridesData)[0]);
+  var rData = [];
+  // a=(ridesData);
+  // console.log((a[0].id));
+  var rObj = {
+    id: 966,
+    origin_station_code: 4,
+    station_path: [23, 31, 49, 57, 63, 78, 80],
+    destination_station_code: 90,
+    date: "02/10/2022 01:53 PM",
+    map_url: "https://picsum.photos/200",
+    state: "Andaman and Nicobar Islands",
+    city: "Port Blair"
+  };
+  for (var i = 0; i < ridesData.length; i++) {
+    rObj = {
+      id: Object.values(ridesData)[i].id,
+      origin_station_code: Object.values(ridesData)[i].origin_station_code,
+      station_path: Object.values(ridesData)[i].station_path,
+      destination_station_code: Object.values(ridesData)[i].destination_station_code,
+      date: Object.values(ridesData)[i].date,
+      map_url: Object.values(ridesData)[i].map_url,
+      state: Object.values(ridesData)[i].state,
+      city: Object.values(ridesData)[i].city
+    }
+    rData.push(rObj);
+  }
+  // console.log(rData[0].destination_station_code);
+
   return (
     <Router>
       <Dashborad
-        name={Object.values(userData)[1]}
-        image={Object.values(userData)[2]}
+        name={uData.name}
+        image={uData.url}
       />
       <Navbar filterState={filterState} setFilterState={setFilterState} />
       <NavbarSort filterState={filterState} />
